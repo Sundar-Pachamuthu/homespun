db725d823a7b81d91904a118474ed3992be4ef0a
Sundar-Pachamuthu <sundarkarthick574@gmail.com>
2024-03-04 19:55:48 +0530

    jh

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Hero.css';
+import logo from '../Assets/logo_big.png';
 
 const Hero = () => {
   return (
     <div className='hero'>
         <div className='hero-left' >
           <h1>here we will display front banner </h1>
+        </div>
+        <div className="hero-right">
+          <img src={logo} alt="" />
         </div>
     </div>
   )
--- src/Components/popular/Popular.jsx
+++ src/Components/popular/Popular.jsx
@@ -7,14 +7,13 @@ import Item from '../Item/Item';
 const Popular = () => {
   return (
     <div className='popular'>
-      <h1>popular in lunch</h1>
+      <h1>popular</h1>
       <hr />
       <div className="popular-item">
         {data_product.map((item,i)=>{
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
         })}
       </div>
-      
     </div>
   )
 }
